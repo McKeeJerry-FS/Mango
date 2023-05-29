cdb9ac74552e029978e41aa848ee3ca43fe0b33a
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2023-05-29 01:02:23 -0400

    Created a Common Response model for CouponAPI

---
+++ Mango.Services.CouponAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto
+{
+    public class ResponseDto
+    {
+        // This DTO is used for Common Response
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
