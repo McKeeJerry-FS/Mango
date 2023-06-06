6a35647b60b2806f44bbc46e1255391bbefebb8a
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2023-06-05 22:11:08 -0400

    Registered BaseService and CouponService

--- Mango.Web/Service/CouponService.cs
+++ Mango.Web/Service/CouponService.cs
@@ -1,6 +1,46 @@
-﻿namespace Mango.Web.Service
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dto;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
 {
-    public class CouponService
+    
+    public class CouponService : ICouponService
     {
+        private readonly IBaseService _baseService;
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;  
+        }
+
+        public Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
